64fde3454d2be4f74f602ecae68fe71b13fb1130
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-04-02 13:52:08 +0530

    Create Greatest.cpp

---
+++ Conditional Assign/Assign1/Greatest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter vale 1:";
+    cin>>a;
+    cout<<"Enter vale 2:";
+    cin>>b;
+
+    if(a>b){
+        cout<<"a is greatest"<< endl;
+    }
+    else{
+        cout<<"b is greatest"<<endl;
+    }
+}
