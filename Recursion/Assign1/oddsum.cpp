a6736a7d5097f980e46db242376499dc8c0a4e6f
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-05-13 12:09:35 +0530

    Update oddsum.cpp

--- Recursion/Assign1/oddsum.cpp
+++ Recursion/Assign1/oddsum.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+int Sum(int a, int b) {
+if(a > b) return 0;
+if(a % 2 == 0) return Sum(a+1,b);
+ return a + Sum(a+2,b);
+}
+
+
+int main() {
+int a;
+cout<<"Enter value of a :";
+cin >> a ;
+int b;
+cout<<"Enter value of b :";
+cin >> b ;
+cout << Sum(a,b) << endl;
+return 0;
+}
