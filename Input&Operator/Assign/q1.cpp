cca69e1a318f098d9db41aa9729a984a0ba4aa97
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-01 16:44:33 +0530

    Create q1.cpp

---
+++ Input&Operator/Assign/q1.cpp
@@ -0,0 +1,11 @@
+int x;
+cout << "Enter first number\n";
+cin >> x; // user will give ‘x’ a value.
+int y, m;
+cout << "Enter second number and value for taking modulus\n";
+cin >> y >> m; // user will give ‘y’ a value.
+int Z = (x * y) % m;
+cout << "Output is: " << Z;
+
+// OUTPUT with values 2,3,6 as input 
+    // Output is 0 
