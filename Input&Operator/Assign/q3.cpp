6151439606cf866307457840722ac5ea255705f9
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-01 16:53:44 +0530

    Create q3.cpp

---
+++ Input&Operator/Assign/q3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int x,y;
+cin>>x>>y;
+x+=y;
+x-=y;
+x%=y;
+cout<<x;
+}
+
+// ===========================OUTPUT=====================
+      // output for the input 2,3 the output will be 2
