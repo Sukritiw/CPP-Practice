f6a148dc94417248574da74fec36b389a8be2df2
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-01 16:52:12 +0530

    Update q1.cpp

--- Input&Operator/Assign/q1.cpp
+++ Input&Operator/Assign/q1.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
 int x;
 cout << "Enter first number\n";
 cin >> x; // user will give ‘x’ a value.
@@ -6,6 +10,7 @@ cout << "Enter second number and value for taking modulus\n";
 cin >> y >> m; // user will give ‘y’ a value.
 int Z = (x * y) % m;
 cout << "Output is: " << Z;
+}
 
-// OUTPUT with values 2,3,6 as input 
-    // Output is 0 
+// =========================OUTPUT=======================
+      // OUTPUT with values 2,3,6 as input THE Output is 0 
