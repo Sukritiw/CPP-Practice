ff1ee58f1c37e92a7264d331b1470a75674e6dfc
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-01 15:46:36 +0530

    Create q5.cpp

---
+++ O&I/Assign1/q5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define PI 3.14159
+using namespace std;
+
+int main(){
+     float r;
+     cout<<" Enter the calue of radius";
+     cin >> r;
+
+     float cir;
+     cir = 2* PI * r;
+     cout<<"Circumference of a circle:"<< " " <<cir;
+
+}
