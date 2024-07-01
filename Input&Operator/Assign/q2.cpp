d0257f674516984c90b28920b0ada50f3c31b5b1
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-01 16:48:34 +0530

    Create q2.cpp

---
+++ Input&Operator/Assign/q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define PI 3.14159
+using namespace std;
+
+int main(){
+int x;
+cout<<"Enter first number\n";
+cin>>x; // user will give 'x' a value.
+int y;
+cout<<"Enter second number\n";
+cin>>y; // user will give 'y' a value.
+cout<<(x!=y)<<" "<<(x>=y);
+}
+ // =============================OUTPUT============================
+              // OUTPUT with input values 3,2 is -> 1 1 
