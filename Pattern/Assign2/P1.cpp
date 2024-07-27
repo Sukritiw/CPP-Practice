7514bc993bf2c5a3669cc6afebdc26d0c166b55b
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-27 21:29:50 +0530

    Update P1.cpp

--- Pattern/Assign2/P1.cpp
+++ Pattern/Assign2/P1.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"No. of rows:";
+    cin>>n;
+   int nst = 1;
+   int nsp = n-1;
+   for(int i = 1; i<=n;i++){
+         for(int j = 1; j<=nsp; j++){
+            cout<<" ";
+   }
+   nsp--;
+     for(int j = 1; j<=nst; j++){
+            cout<<j;
+   }
+   nst+=2;
+   cout<<endl;
+}
+
+}
+
 
