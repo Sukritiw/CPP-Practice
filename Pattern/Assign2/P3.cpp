4ba93f806e45dd8a52b68a34ac1dc175b352a952
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-07-31 16:10:47 +0530

    Update P3.cpp

--- Pattern/Assign2/P3.cpp
+++ Pattern/Assign2/P3.cpp
@@ -5,8 +5,7 @@ int main(){
     int n;
     cout<<"No. of rows:";
     cin>>n;
-//    int nst = 1;
-//    int nsp = n-1;
+
    for(int i = 1; i<=n;i++){
          for(int j = 1; j<=n-i; j++){ 
             cout<<" ";
