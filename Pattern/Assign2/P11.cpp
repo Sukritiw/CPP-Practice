1208f156418bff7e2527895cb42c448f43537aeb
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-08-06 14:36:48 +0530

    Create P11.cpp

---
+++ Pattern/Assign2/P11.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"No. of rows:";
+    cin>>n;
+
+    // Upper half of the butterfly
+    int m = n-1;
+    int nsp = 1;
+    for(int i = m; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            cout<<"* ";
+        }
+        for(int k = 1; k <= nsp; k++){
+            cout<<"  ";
+        }
+        nsp += 2;
+        for(int j = 1; j <= i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    // Lower half of the butterfly
+
+       nsp = 2*m-1;
+
+    for(int i = 1; i <= m; i++){
+        for(int j = 1; j <= i; j++){
+            cout<<"* ";
+        }
+        for(int k = 1; k <= nsp; k++){
+            cout<<"  ";
+        }
+        nsp -= 2;
+        for(int j = 1; j <= i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
