b7049d9ca4a2196e6cc5e05bb639d6b2a70cae83
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-09-26 13:01:25 +0530

    Update q1.cpp

--- Array/Array2/q1.cpp
+++ Array/Array2/q1.cpp
@@ -1,1 +1,31 @@
 
+#include<iostream>
+using namespace std;
+
+int main(){
+  
+
+  int n;
+  cout<<"Enter the size of an array:";
+  cin>>n;
+
+  int arr[n];
+  cout<<"Enter the elements of an array:";
+   for(int i = 0; i<n; i++) {
+        cin>>arr[i];  
+    }
+
+    int x ;
+    cout<<"enter value of x:";
+    cin>>x;
+
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] > x) {
+            ++count;
+        }
+    }
+     cout<<"number of element greater than"<<" "<< x <<" are"<< " " <<count;
+    
+    
+}
