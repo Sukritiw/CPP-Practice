af049d894ff4c07234e2b84c6b2899563449bf97
Sukriti Trivedi <125355204+Sukritiw@users.noreply.github.com>
2024-10-10 23:06:08 +0530

    Update q1.cpp

--- Strings/Assign1/q1.cpp
+++ Strings/Assign1/q1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
 
+
+using namespace std;
+
+int main() {
+    
+    string str = ""; //empty string
+   
+    int n;
+    cout<<"Enter the length of the string:";
+    cin>>n;
+
+    for(int i = 0; i<n;i++){
+        char ch;
+        cin>>ch;
+        str.push_back(ch);
+    }
+     cout<<str<<endl;
+    for(int i = 0; i<n; i++){
+        if(i%2 == 0)
+        str[i] = '#';
+    }
+    cout<<str<<endl;
+}
